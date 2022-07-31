154482a7182bc5d853db16e136e90bfa6438c0e6
JayaArmstrong <mew1281@hotmail.com.au>
2022-07-31 18:45:10 +1000

    Fixed devmode

--- public/emoji-describer/config.js
+++ public/emoji-describer/config.js
@@ -1,6 +1,6 @@
 const config = {
     nameOfGame: "Emoji Describer",
     landingPage: "landingPage",
-    devMode: true,
+    devMode: false,
     socketURL: "wss://uuql9ukc55.execute-api.ap-southeast-2.amazonaws.com/Test"
 }

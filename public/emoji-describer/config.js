10590dacccf04b844af825efc6f93ba87d5a35f1
JayaArmstrong <mew1281@hotmail.com.au>
2023-03-28 22:17:13 +1100

    Update config.js

--- public/emoji-describer/config.js
+++ public/emoji-describer/config.js
@@ -9,7 +9,7 @@ const config = {
     lobbyPlayerColours: ['text-red-600', 'text-purple-600', 'text-green-600', 'text-orange-600', 'text-sky-600', 'text-fuchsia-600', 'text-emerald-600', 'text-cyan-600', 'text-pink-600', 'text-yellow-600', 'text-teal-600', 'text-lime-600', 'text-violet-600', 'text-amber-600', 'text-indigo-600', 'text-rose-600'],
     emojiSearchLimit: 18,
     lobbyCodeFormat: /^[A-Z]{5}$/,
-    animationsEnabled: false,
+    animationsEnabled: true,
     bgAnimationGap: {
         lower: 5,
         upper: 10
